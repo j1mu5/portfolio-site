4d90a467008e41de830ec2cb960ca6fb46e0e9ae
James Chance <38363488+j1mu5@users.noreply.github.com>
2021-04-23 19:28:55 +0100

    removed parcel
    removed parcel

--- main.js
+++
@@ -1,7 +0,0 @@
-// import a CSS module
-import classes from './main.css';
-
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import 'animate.css'
