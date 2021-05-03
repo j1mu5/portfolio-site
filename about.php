be6a807fdc6df5948c568dc6296f2e3fc482c0d8
James Chance <38363488+j1mu5@users.noreply.github.com>
2021-05-03 18:43:07 +0100

    Updated, about me section
    Updated, about me section

--- about.php
+++ about.php
@@ -4,9 +4,9 @@
     <div class="row">
       <div class="col-md-6 col-xs-12 col-sm-12 d-flex flex-column justify-content-center">
         <h1 class="my-logo display-5 fw-bold text-light ">About Me</h1>
-        <p class="text-light fs-5 mt-4">When I am not building website or studing for a degree in computer science I love to spending time with my wife a daughter. I'm a huge Leicester Tigers fan and spend any free time I have catching up on the
+        <p class="text-light fs-5 mt-4">When I am not building website or studying for a degree in computer science I love to spending time with my wife and daughter. I'm a huge Leicester Tigers fan and spend any free time I have catching up on the
           rugby. If you
-          would like to work with me then please use the contact form below, alternatively you could reach out to me on LinkedIn</p>
+          would like to work with me then please use the contact form below, alternatively you could reach out to me on Twitter.</p>
       </div>
       <div class="col-md-6 col-xs-12 col-sm-12">
         <img src="me.jpg" alt="profile photo of me" class="img-fluid d-block rounded shadow-lg img-thumbnail w-sm-75 float-end w-xs-100">
